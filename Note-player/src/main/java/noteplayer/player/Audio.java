3f3443bd712cfd3a58554b9a63853ffa62b076eb
Teo44 <43036149+Teo44@users.noreply.github.com>
2019-04-01 16:10:46 +0300

    Added basic audio playback

---
+++ Note-player/src/main/java/noteplayer/player/Audio.java
@@ -0,0 +1,46 @@
+package noteplayer.player;
+
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class Audio  {
+    
+    private AudioInputStream stream;
+    private Clip clip;
+    private File audioFile;
+    
+    public void play(String file)  {
+        
+        // Stop any ongoing playback before starting the new one
+        if (clip != null)   {
+            clip.stop();
+        }
+        
+        try {
+            audioFile = new File(file);
+            stream = AudioSystem.getAudioInputStream(audioFile);
+            clip = AudioSystem.getClip();
+            clip.open(stream);
+            clip.start();
+        } catch (Exception e)   {
+            System.out.print("Could not play file: ");
+            System.out.println(e);
+        }
+        
+    }
+    
+    public void pausePlayback()  {
+        clip.stop();
+    }
+    
+    public void unpausePlayback()   {
+        clip.start();
+    }
+    
+    
+    
+    
+    
+}
---
+++ Note-player/src/main/java/noteplayer/player/Main.java
@@ -0,0 +1,30 @@
+
+package noteplayer.player;
+
+import java.util.*;
+
+
+public class Main {
+    
+    public static void main(String[] args)  {
+        System.out.print("Enter a filename to play it, ");
+        System.out.println("\"pause\" or \"unpause\" to control playback:");
+        
+        
+        Scanner scanner = new Scanner(System.in);
+        Audio audio = new Audio();
+        
+        
+        while (true)    {
+            String input = scanner.nextLine();
+            
+            if (input.equals("quit")) break;
+            else if (input.equals("pause")) audio.pausePlayback();
+            else if (input.equals("unpause")) audio.unpausePlayback();
+            else audio.play(input);
+            
+        }
+        
+    }
+    
+}
